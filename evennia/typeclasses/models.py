6da3723781dec6bfc427967a3df94095cff74fa8
Jeremy <colearkenach@gmail.com>
2020-03-23 18:46:28 -0400

    ded

--- evennia/typeclasses/models.py
+++ evennia/typeclasses/models.py
@@ -186,7 +186,7 @@ class TypedObject(SharedMemoryModel):
     db_key = models.CharField("key", max_length=255, db_index=True)
 
     # ImageField
-    db_image = models.ImageField(upload_to='character_image')
+    db_media = models.ImageField(upload_to='character_image')
 
     # This is the python path to the type class this object is tied to. The
     # typeclass is what defines what kind of Object this is)
