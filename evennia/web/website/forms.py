1e091bad39e4bd8bdcd34a67596ee99f44dabdde
Jeremy <colearkenach@gmail.com>
2020-03-23 22:29:49 -0400

    a

--- evennia/web/website/forms.py
+++ evennia/web/website/forms.py
@@ -143,10 +143,10 @@ class CharacterForm(ObjectForm):
         model = class_from_module(settings.BASE_CHARACTER_TYPECLASS)
 
         # Allow entry of the 'key' field
-        fields = ("db_key",)
+        fields = ("db_key", "db_age",)
 
         # Rename 'key' to something more intelligible
-        labels = {"db_key": "Name"}
+        labels = {"db_key": "Name", "db_age": "Age"}
 
     # Fields pertaining to configurable attributes on the Character object.
 
