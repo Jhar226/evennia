45b826442bfbe5f5f58388e3c2e0784ccd192ffc
Jeremy <colearkenach@gmail.com>
2020-03-27 23:00:24 -0400

    ss

--- evennia/web/website/forms.py
+++ evennia/web/website/forms.py
@@ -143,22 +143,22 @@ class CharacterForm(ObjectForm):
         model = class_from_module(settings.BASE_CHARACTER_TYPECLASS)
 
         # Allow entry of the 'key' field
-        fields = ("db_key",)
+        fields = ("db_key", "image", "age")
 
         # Rename 'key' to something more intelligible
         labels = {"db_key": "Name"}
 
     # Fields pertaining to configurable attributes on the Character object.
 
-    image = forms.CharField(
-        label="Image",
+    image = forms.URLField(
+        initial='http://'
         required=False,
         help_text="Your character image."
 	)
 
     age = forms.IntegerField(
         label="Age",
-        min_value=0, max_value=9000000,
+        required=False,
         help_text="How old is this character in years?",
     )
 
