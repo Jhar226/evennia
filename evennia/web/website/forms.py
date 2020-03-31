5fa290aa6b6208a51bcdd425e7c79488fb589c6f
Jeremy <colearkenach@gmail.com>
2020-03-31 00:00:17 -0400

    ee

--- evennia/web/website/forms.py
+++ evennia/web/website/forms.py
@@ -5,8 +5,6 @@ from django.forms import ModelForm
 from django.utils.html import escape
 from evennia.utils import class_from_module
 
-import urllib, cStringIO
-
 class EvenniaForm(forms.Form):
     """
     This is a stock Django form, but modified so that all values provided
