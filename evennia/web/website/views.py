5b2828bbd71c66d7ea60f71aabe4575e901d9934
Jeremy <colearkenach@gmail.com>
2020-03-31 05:19:31 -0400

    Pee

--- evennia/web/website/views.py
+++ evennia/web/website/views.py
@@ -719,7 +719,7 @@ class CharacterDetailView(CharacterMixin, ObjectDetailView):
 
     # -- Evennia constructs --
     # What attributes to display for this object
-    attributes = ["name", "image", "age", "desc"]
+    attributes = ["name", "image", "age", "race", "archetype", "desc"]
     access_type = "view"
 
     def get_queryset(self):
